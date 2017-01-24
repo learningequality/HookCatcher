3024d4d1323c4a1a95fbad780593864cad08a114
Ming Dai <daiwilliam@live.com>
2017-01-24 14:16:07 -0800

    database restart and BrowserStack request

--- HookCatcher/models.py
+++ HookCatcher/models.py
@@ -9,33 +9,31 @@ import uuid
 @python_2_unicode_compatible
 class State(models.Model):
 	id = models.AutoField(primary_key=True)
-	state_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 	state_name = models.CharField(max_length=200)
 	state_desc = models.TextField()
 	state_json = models.TextField()
-	target = models.CharField(max_length=200)
-	target_type = models.CharField(max_length=200)
-	target_id = models.CharField(max_length=200)
+	github_source_type = models.CharField(max_length=200)
+	github_source_id = models.CharField(max_length=200)
 	def __str__(self):
 		return self.state_name
 
-@python_2_unicode_compatible
-class Diff(models.Model):
-	id = models.AutoField(primary_key=True)
-	before = models.CharField(max_length=200)
-	after = models.CharField(max_length=200)
-	url = models.URLField(max_length=200)
-	diff_percent = models.DecimalField(max_digits=6,decimal_places=3,default=Decimal('0.00'))
-	def __str__(self):
-		return '%s diff %s' % (self.before, self.after)
 
 @python_2_unicode_compatible
 class Image(models.Model):
-	id = models.AutoField
-	url = models.URLField(max_length=200)
-	browser = models.CharField(max_length=200)
-	state = models.ForeignKey(State, on_delete=models.CASCADE)
+	id = models.AutoField(primary_key=True)
+	state_img_url = models.URLField(max_length=200)	
+	browser_type = models.CharField(max_length=200)
+	state = models.ForeignKey(State, on_delete=models.CASCADE)#many Images to one State (for multiple browsers)
 	def __str__(self):
 		return '%s_img' % self.state.state_name
 
 
+@python_2_unicode_compatible
+class Diff(models.Model):
+	id = models.AutoField(primary_key=True)
+	test_state = models.ForeignKey(Image, related_name='where_isTest', on_delete=models.CASCADE)#many Diffs to one Image
+	control_state = models.ForeignKey(Image, related_name='where_isControl', on_delete=models.CASCADE)#many Diffs to one Image
+	diff_img_url = models.URLField(max_length=200)
+	diff_percent = models.DecimalField(max_digits=6,decimal_places=3,default=Decimal('0.00'))
+	def __str__(self):
+		return '%s diff %s' % (self.test_state, self.control_state)
--- HookCatcher/views.py
+++ HookCatcher/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-import json
+import json, requests
 # must import models and save to models
 @csrf_exempt
 def index(request):
-	if request.method == 'POST':
+	
+
+	'''if request.method == 'POST':
 		gitData = request.body
 		gitJSON = json.loads(gitData)
 		print '\nRaw Data: "%s"\n' % gitJSON['zen']
+	
+	'''
+	headers = {
+	    'Content-Type': 'application/json',
+	    'Accept': 'application/json',
+	}
+
+	data = '{"url": "https://learningequality.org/about/team/", "callback_url": "http://82019d72.ngrok.io", "win_res": "1024x768", "mac_res": "1920x1080", "quality": "compressed", "wait_time": 60, "orientation": "portrait", "browsers":[{"os": "Windows", "os_version": "7", "browser_version": "9.0", "browser": "ie"}]}'
+
+	postRequest = requests.post('https://www.browserstack.com/screenshots', headers=headers, data=data, auth=('mingdai1', 'dfTNku6CERcRaExPs6KF'))
+	print 'Request Text: "%s"\n' % postRequest.text
+
+	bsReply = request.body
+	JSONbsReply = json.loads(bsReply)
+	print '\nRequest Body: "%s"\n' % JSONbsReply
+
 	return render(request, 'index.html',)
 
 # Create your views here.
