0d5cdb8265a884f08b68176012d3a991262c6c7b
Ming Dai <daiwilliam@live.com>
2017-02-02 16:26:56 -0800

    implement switch branch func

--- screenshot/pythonSH.py
+++ screenshot/pythonSH.py
@@ -1,34 +1,49 @@
 import sh
 #sh.cd('..')
-import path
+from os import path
 
-KOLIBRI_DIR = path.abspath("../kolibri")
+KOLIBRI_DIR = path.abspath("../../../Github/kolibri")
 IMAGES_DIR = "gen-images"
 
 
-#print(sh.ls('-l'))
-sh.phantomjs('capture.js', 'urlstr', ...)
-#sh.compare('')
-
-
-#given an array of urls and branches automate screenshotting of them between different branches and create diff img
-
-def genScreenshot(name, url):
+#take screenshot of a url with PhantomJS and save the file into images folder
+	#url = the url that the screenshot is taken
+	#fileName = name of the file for the screenshot, represents the state name and the gitSource version
+def genScreenshot(url, fileName):
 	screenshotURL = url
-	screenshotName = '{0}.jpeg'.format(name)
-	sh.phantomjs('capture.js', url, path.join(IMAGES_DIR, screenshotName))
+	screenshotName = '{0}.jpeg'.format(fileName)
+	print(screenshotName)
+	sh.phantomjs('capture.js', url, '{0}/{1}'.format(IMAGES_DIR, screenshotName))
 
+
+
+	
+#switch the kolibri branch so that the state of kolibri changes
+	#gitBranch = the name of the git branch for the kolibri instance ot be switched to
+def switchBranch(gitBranch):
+	kolibri_git_dir = path.abspath(path.join(KOLIBRI_DIR, '.git'))
+	print('kolibri dir: ' + kolibri_git_dir)
+	working_dir = path.abspath('')
+	print('working dir:' + working_dir)
+	print(sh.git('--git-dir', kolibri_git_dir, '--work-tree', working_dir, 'status'))
+
+#pass in two file names and generate a new fle
+	#img1 = the file name of the image being compared by
+	#img2 = the file name of the image being compared to
+def createDiff(img1, img2):
 	comparisonIMG = 'exploreMasterLarge.jpeg'
 	diffIMG = comparisonIMG + '_' + screenshotName + '.png'
 	sh.compare(comparisonIMG + ' ' + screenshotName + ' ' + diffIMG)
-	
 
+gitBranches = ['test-pr', 'test-master']
 
 states = [('explore', 'http://localhost:8000/learn/#/explore/5b1e904335ab4dfda82e3e37735262c5'), 
 			('learn', 'http://localhost:8000/learn/#/learn/5b1e904335ab4dfda82e3e37735262c5')]
 
-for stateTuple in states:
-	genScreenshot(stateTuple[0], stateTuple[1])
+for branchToggle in gitBranches:
+	switchBranch(branchToggle)
+	for stateTuple in states:
+		genScreenshot(stateTuple[1], '{0}_{1}'.format(branchToggle, stateTuple[0]))
 
 # In [4]: git_dir = os.path.abspath(os.path.join('HookCatcherProj', '.git'))
 
